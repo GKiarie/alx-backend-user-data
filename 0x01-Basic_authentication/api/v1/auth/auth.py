3ccbeb83a3434a6654e817b0b5daa8d6420e3d1f
GKiarie <igitaukiarie@gmail.com>
2023-08-08 17:19:53 +0300

    validating requests

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -33,7 +33,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Returns None - request"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None - request"""
