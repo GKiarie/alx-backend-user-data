544075251167d987220f983e2b3410d0de0748c6
GKiarie <igitaukiarie@gmail.com>
2023-08-08 18:21:04 +0300

    Base64 decoding ftn

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,17 @@ class BasicAuth(Auth):
         base64_part = authorization_header[len('Basic '):]
 
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """method returns decoded value of Base64 string"""
+        if base64_authorization_header is None or not isinstance(
+             base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_bytes.decode('utf-8')
+            return decoded_value
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
